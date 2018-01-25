9a0d084b0c1981c2fd5c2a3e7643469e4a4ee98e
K8948 <K8948@student.jamk.fi>
2018-01-25 18:46:59 +0200

    first

---
+++ week4/ItMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace week4
+{
+    public static class ItMath
+    {
+        public static bool IsNumber(string input)
+        {
+            NumberFormatInfo nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ",";
+            int kek = Int32.Parse(input);
+            return true;
+        }
+    }
+}
--- week4/Program.cs
+++ week4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace week4
 {
@@ -10,6 +11,15 @@ namespace week4
     {
         static void Main(string[] args)
         {
+            CultureInfo culture1 = new CultureInfo("fi-FI",false);
+            Console.WriteLine("The current culture is {0}", culture1.Name);
+            Console.WriteLine("Culture Name:    {0}", culture1.Name);
+            Console.WriteLine("User Overrides:  {0}", culture1.UseUserOverride);
+            Console.WriteLine("Currency Symbol: {0}", culture1.NumberFormat.CurrencySymbol);
+            Console.WriteLine("Decimal Separator: {0}\n", culture1.NumberFormat.NumberDecimalSeparator);
+
+            ItMath.IsNumber(Console.ReadLine());
+            Console.ReadKey(true);
         }
     }
 }
